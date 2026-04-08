e1ed730e83e9215954808c46bac0fb87d9820640
hsrobotics <hsrobotics@HS-Robotics-03.ad.gccsmi.org>
2026-04-07 20:43:55 -0400

    Up-to-Date

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -244,9 +244,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 ),
                 new PPHolonomicDriveController(
                     // PID constants for translation
-                    new PIDConstants(10, 0, 0),
+                    new PIDConstants(8, 0, 0.1),
                     // PID constants for rotation
-                    new PIDConstants(11, 0, 0)
+                    new PIDConstants(10.5, 0, 0.2)
                 ),
                 config,
                 // Assume the path needs to be flipped for Red vs Blue, this is normally the case
